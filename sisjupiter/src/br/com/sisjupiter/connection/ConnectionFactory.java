e18fd028b5818c1ea04d628fcbb2568ef9fd924c
wagner <wagner@wcamargo>
2018-03-17 23:27:09 -0300

    logoti

--- sisjupiter/src/br/com/sisjupiter/connection/ConnectionFactory.java
+++ sisjupiter/src/br/com/sisjupiter/connection/ConnectionFactory.java
@@ -7,17 +7,23 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    //CONEXAO LOCAL
-    private static final String url = "jdbc:mysql://localhost:3306/sisjupiter001";
-    private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String usuario = "root";
-    private static final String senha = "admin";
-    
+//    //CONEXAO LOCAL
+//    private static final String url = "jdbc:mysql://localhost:3306/sisjupiter001";
+//    private static final String driver = "com.mysql.jdbc.Driver";
+//    private static final String usuario = "root";
+//    private static final String senha = "admin";
+//    
 //    //CONEXAO REMOTA
 //    private static final String url = "jdbc:mysql://192.175.112.170:3306/diagsdee_001";
 //    private static final String driver = "com.mysql.jdbc.Driver";
 //    private static final String usuario = "diagsdee_001";
 //    private static final String senha = "87hfds&dsa123";
+    
+    //CONEXAO REMOTA
+    private static final String url = "jdbc:mysql://localhost:3306/diagsdee_001";
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String usuario = "diagsdee_001";
+    private static final String senha = "87hfds&dsa123";
 
     public static Connection getConnection() throws SQLException {
 
